785bdef7d691fabc4bce6dfaab9cdf6ac5317ab2
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-08-29 14:12:07 -0500

    finished component with useReducer hook

--- src/component/CounterWithReducer.js
+++ src/component/CounterWithReducer.js
@@ -1,13 +1,31 @@
+import React, { useReducer } from 'react'
 import styles from './CounterWithReducer.module.css'
 
+const reducer = (state, action) => {
+  // if (action.type == 'DECREMENT') return { count: state.count - 1 }
+  // if (action.type == 'INCREMENT') return { count: state.count + 1 }
+  // console.log('return default state')
+  // return state
+
+  switch (action.type) {
+    case 'DECREMENT':
+      return { count: state.count - 1 }
+    case 'INCREMENT':
+      return { count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
 const CounterWithReducer = () => {
-    const decrement = () => {
-        console.log("hello from decrement")
-    } 
-    const increment = () => {
-        console.log("hello from decrement")
-    } 
-    const count  = 0;
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT' })
+  }
+  const increment = () => {
+    dispatch({ type: 'INCREMENT' })
+  }
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Hello from Counter With State</h1>
@@ -15,7 +33,7 @@ const CounterWithReducer = () => {
         <button onClick={decrement} className={styles.decrement}>
           -
         </button>
-        <span className={styles.span}>{count}</span>
+        <span className={styles.span}>{state.count}</span>
         <button onClick={increment} className={styles.increment}>
           +
         </button>
